afe0f17774e1f5474e7abd3e35085e15bcabf583
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-09-08 15:04:09 +0200

    yaxis

--- src/config/adapters/dhis2highcharts/yAxis.js
+++ src/config/adapters/dhis2highcharts/yAxis.js
@@ -2,26 +2,38 @@ import arrayClean from 'd2-utilizr/lib/arrayClean';
 import isNumeric from 'd2-utilizr/lib/isNumeric';
 import isString from 'd2-utilizr/lib/isString';
 
+const DEFAULT_MIN_VALUE = 0;
+
+function getMinValue(layout) {
+    return isNumeric(layout.rangeAxisMaxValue) ? layout.rangeAxisMaxValue : DEFAULT_MIN_VALUE;
+}
+
+function getMaxValue(layout) {
+    return isNumeric(layout.rangeAxisMaxValue) ? layout.rangeAxisMaxValue : undefined;
+}
+
+function getSteps(layout) {
+    return isNumeric(layout.rangeAxisSteps) ? layout.rangeAxisSteps : undefined;
+}
+
+function getDecimals(layout) {
+    return isNumeric(layout.rangeAxisDecimals) ? layout.rangeAxisDecimals : undefined;
+}
+
 function getTitle(layout) {
-    const title;
-
-    if (isString(layout.rangeAxisTitle)) {
-        title = {
-            text: layout.rangeAxisTitle
-        };
-    }
-
-    return title;
+    return isString(layout.rangeAxisTitle) ? {
+        text: layout.rangeAxisTitle
+    } : undefined;
 }
 
 function getTargetLine(layout) {
-    const targetLine;
+    let targetLine;
 
     if (isNumeric(layout.targetLineValue)) {
         targetLine = {
             value: layout.targetLineValue,
             width: 1,
-            color: '#808080'
+            color: '#333'
         };
 
         if (isString(layout.targetLineTitle)) {
@@ -35,13 +47,13 @@ function getTargetLine(layout) {
 }
 
 function getBaseLine(layout) {
-    const baseLine;
+    let baseLine;
 
     if (isNumeric(layout.baseLineValue)) {
         baseLine = {
             value: layout.baseLineValue,
             width: 1,
-            color: '#808080'
+            color: '#333'
         };
 
         if (isString(layout.baseLineTitle)) {
@@ -55,8 +67,62 @@ function getBaseLine(layout) {
 }
 
 export default function getYAxis(layout) {
-    return {
-        title: getTitle(layout),
+    const yAxis = {
+        min: getMinValue(layout)
+    };
+
+    // max value
+    const maxValue = getMaxValue(layout);
+
+    if (isDefined(maxValue)) {
+        yAxis.max = maxValue;
+    }
+
+    // tick steps
+    const steps = getSteps(layout);
+
+    if (isDefined(amount)) {
+        yAxis.tickAmount = steps;
+    }
+
+    // decimals
+    const decimals = getDecimals(layout);
+
+    if (isDefined(amount)) {
+        yAxis.tickAmount = amount;
+    }
+
+    // title
+    const title = getTitle(layout);
+
+    if (isDefined(title)) {
+        yAxis.title = title;
+    }
+
+    // target/base line
+    const lines = arrayClean([getTargetLine(layout), getBaseLine(layout)]);
+
+    if (lines.length) {
+        yAxis.plotLines = lines;
+    }
+
+
+
+
+
+    // title
+    const title = getTitle(layout);
+
+    if (isDefined(title)) {
+        yAxis.title = title;
+    }
+
+    // target/base line
+    const targetLine = getTargetLine(layout);
+
+    if (targetLine)
         plotLines: arrayClean([getTargetLine(layout), getBaseLine(layout)])
     };
+
+    //
 }
