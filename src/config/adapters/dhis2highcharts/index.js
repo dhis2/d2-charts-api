4878b28f1e1a40ec1b327ad15105cc4ee1c33dec
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-09-08 14:18:16 +0200

    regression + series

--- src/config/adapters/dhis2highcharts/index.js
+++ src/config/adapters/dhis2highcharts/index.js
@@ -1,22 +1,35 @@
+import isDefined from 'd2-utilizr/lib/isDefined';
 import getXAxis from './xAxis';
 import getYAxis from './yAxis';
+import getSeries from './series';
 import getTitle from './title';
 
-export default function adapter(layout, response, extraLayout) {
-    return {
+export default function adapter(store, layout, response, extraLayout) {
+    const config = {
 
         // type
         chart: {
             type: layout.type
         },
 
-        // title
-        title: getTitle(layout),
-
         // x-axis
         xAxis: getXAxis(layout, response),
 
         // y-axis
-        yAxis: getYAxis(layout)
+        yAxis: getYAxis(layout),
+
+        // series
+        series: getSeries(store),
+
+
     };
+
+    // title
+    const title = getTitle(layout);
+
+    if (isDefined(title)) {
+        config.title = title;
+    }
+
+    return config;
 }
