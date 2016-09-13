a11bef9ca3955f9079ff99fbf474b2ce0bc4db22
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-09-13 16:26:58 +0200

    index export

--- src/index.js
+++ src/index.js
@@ -2,8 +2,15 @@ import Store from './store';
 import Config from './config';
 import Chart from './chart';
 
+function createChart(data, layout, el) {
+    const store = new Store({ data });
+    const config = new Config({ store, layout, el });
+    Chart({ config });
+}
+
 export {
     Store,
     Config,
-    Chart
+    Chart,
+    createChart
 };
