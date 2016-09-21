e613392277aa06bc089b67ec82eaa1c4c9701c80
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-09-21 15:28:05 +0200

    pie label formatter

--- src/config/adapters/dhis_highcharts/series/pie.js
+++ src/config/adapters/dhis_highcharts/series/pie.js
@@ -13,9 +13,8 @@ export default function (series, store, layout, isStacked, colors) {
         })),
         dataLabels: {
             enabled: true,
-            formatter: function() {
+            formatter: function() {
                 return '<span style="font-weight:normal">' + this.point.name + '</span><br/>' + this.y + '<span style="font-weight:normal"> (' + this.percentage.toFixed(1) + ' %)</span>';
-                return '<span style="font-weight:normal">' + this.point.name + '</span>: ' + this.y + '<span style="font-weight:normal"> (' + this.percentage.toFixed(1) + ' %)</span>';
             }
         }
     }];
