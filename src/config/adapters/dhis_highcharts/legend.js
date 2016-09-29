e8ea1f86b188bdda54dc22f1507824fa3aaf52d9
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-09-29 14:14:07 +0200

    legend style

--- src/config/adapters/dhis_highcharts/legend.js
+++ src/config/adapters/dhis_highcharts/legend.js
@@ -1,4 +1,4 @@
-const DEFAULT_FONT_SIZE = '11px';
+const DEFAULT_FONT_SIZE = '13px';
 
 const DASHBOARD_FONT_SIZE = '10px';
 
@@ -10,8 +10,9 @@ export default function (layout, dashboard) {
     } : {
         symbolWidth: 11,
         symbolHeight: 11,
-        labelFormatter: function() {
-            return '<span style="font-size:' + fontSize + '; font-weight:normal; color:#333; text-shadow:0 0 #aaa">' + this.name + '</span>';
+        itemStyle: {
+            fontSize: fontSize,
+            fontWeight: 'normal'
         }
     };
 }
