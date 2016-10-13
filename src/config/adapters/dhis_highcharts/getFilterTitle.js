0afa5c4d7af7f8647adaf4a93b72ebc4cf9f9a74
Jan Henrik Overland <janhenrik.overland@gmail.com>
2016-10-13 10:05:51 +0200

    filter title vulnerability fixed

--- src/config/adapters/dhis_highcharts/getFilterTitle.js
+++ src/config/adapters/dhis_highcharts/getFilterTitle.js
@@ -1,13 +1,17 @@
+import isArray from 'd2-utilizr/lib/isArray';
+
 export default function (layout, metaData) {
     let title = '';
 
-    layout.filters.forEach((dimension, index, array) => {
-        metaData[dimension.dimension].forEach((id, index, array) => {
-            title += metaData.names[id] + (index < array.length - 1 ? ', ' : '');
-        });
+    if (isArray(layout.filters)) {
+        layout.filters.forEach((dimension, index, array) => {
+            metaData[dimension.dimension].forEach((id, index, array) => {
+                title += metaData.names[id] + (index < array.length - 1 ? ', ' : '');
+            });
 
-        title +=  (index < array.length - 1 ? ' - ' : '');
-    });
+            title += (index < array.length - 1 ? ' - ' : '');
+        });
+    }
 
     return title;
 }
