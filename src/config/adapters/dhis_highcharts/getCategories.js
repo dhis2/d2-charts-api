05d10aec7af3c8f1942c8956212815231c4e338b
Ilya Nee <neeilya@mail.ru>
2017-02-13 11:49:42 +0100

    Adjusted fetching categories

--- src/config/adapters/dhis_highcharts/getCategories.js
+++ src/config/adapters/dhis_highcharts/getCategories.js
@@ -4,13 +4,13 @@ export default function (store, layout) {
     const metaData = store.data.metaData;
     let categories;
 
-    const ids = metaData[layout.rows[0].dimension];
+    const ids = metaData.dimensions[layout.rows[0].dimension];
 
     if (isArray(ids) && ids.length) {
         categories = [];
 
         ids.forEach(id => {
-            categories.push(metaData.names[id]);
+            categories.push(metaData.items[id].name);
         });
     }
 
