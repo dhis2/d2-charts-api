2a3dfeb615ab089624daae1c6d9eeb81e6d9310d
Jan Henrik Overland <janhenrik.overland@gmail.com>
2017-04-20 14:23:37 +0200

    svg exporting

--- src/config/generators/highcharts/index.js
+++ src/config/generators/highcharts/index.js
@@ -3,14 +3,12 @@ import HM from 'highcharts-more';
 import HSG from 'highcharts-solid-gauge';
 import HNDTD from 'highcharts-no-data-to-display';
 import HE from 'highcharts-exporting';
-import HOE from 'highcharts-offline-exporting';
 
 // apply
 HM(H);
 HSG(H);
 HNDTD(H);
 HE(H);
-HOE(H);
 
 export default function (config, el) {
     if (config) {
