9dc60acfbcffd4de4d12a159986703f9de16081b
Jan Henrik Overland <janhenrik.overland@gmail.com>
2017-04-20 13:44:42 +0200

    hc exporting

--- src/config/generators/highcharts/index.js
+++ src/config/generators/highcharts/index.js
@@ -2,11 +2,13 @@ import H from 'highcharts';
 import HM from 'highcharts-more';
 import HSG from 'highcharts-solid-gauge';
 import HNDTD from 'highcharts-no-data-to-display';
+import HE from 'highcharts-exporting';
 
 // apply
 HM(H);
 HSG(H);
 HNDTD(H);
+HE(H);
 
 export default function (config, el) {
     if (config) {
