e750df895553d6ae023bd979fcbd765bd6247916
Jan Henrik Overland <janhenrik.overland@gmail.com>
2017-04-20 13:44:42 +0200

    HC upgrade + exporting modules

--- src/config/generators/highcharts/index.js
+++ src/config/generators/highcharts/index.js
@@ -2,11 +2,15 @@ import H from 'highcharts';
 import HM from 'highcharts-more';
 import HSG from 'highcharts-solid-gauge';
 import HNDTD from 'highcharts-no-data-to-display';
+import HE from 'highcharts-exporting';
+import HOE from 'highcharts-offline-exporting';
 
 // apply
 HM(H);
 HSG(H);
 HNDTD(H);
+HE(H);
+HOE(H);
 
 export default function (config, el) {
     if (config) {
