61695027bbd04eb80cc9bf44dbb1df93ea64c9f1
Edoardo Sabadelli <edoardo@dhis2.org>
2017-05-24 16:19:04 +0200

    Bugfix: app crashes when using data items as filter in EV

--- src/config/adapters/dhis_highcharts/getFilterTitle.js
+++ src/config/adapters/dhis_highcharts/getFilterTitle.js
@@ -6,12 +6,32 @@ export default function (filters, metaData) {
     if (isArray(filters) && filters.length) {
         title = '';
 
-        filters.forEach((dimension, index, array) => {
-            metaData.dimensions[dimension.dimension].forEach((id, index, array) => {
-                title += metaData.items[id].name + (index < array.length - 1 ? ', ' : '');
-            });
+        let filterItems,
+            filterItemsIndex;
 
-            title += (index < array.length - 1 ? ' - ' : '');
+        filters.forEach((dimension, index, array) => {
+            filterItems = metaData.dimensions[dimension.dimension];
+            filterItemsIndex = 0;
+
+            if (isArray(filterItems)) {
+                for (let id of filterItems) {
+                    // if the value is present in items take the name to show from there
+                    if (metaData.items[id]) {
+                        title += metaData.items[id].name + (filterItemsIndex < filterItems.length - 1 ? ', ' : '');
+                    }
+                    // otherwise use the values directly
+                    // this is a temporary fix to avoid app crashing when using filters with data items in EV
+                    else {
+                        title += metaData.items[dimension.dimension].name + ': ' + filterItems.join(', ');
+
+                        break;
+                    }
+
+                    filterItemsIndex++;
+                }
+
+                title += (index < array.length - 1 ? ' - ' : '');
+            }
         });
     }
 
