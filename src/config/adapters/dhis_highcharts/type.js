cad66a80c0dd0dd22b0f25350d81943a97344d1b
Jan Henrik Overland <jan@dhis2.org>
2018-10-26 10:27:07 +0200

    Update chart type constants

--- src/config/adapters/dhis_highcharts/type.js
+++ src/config/adapters/dhis_highcharts/type.js
@@ -1,21 +1,31 @@
 import arrayContains from 'd2-utilizr/lib/arrayContains';
 
-// Note: IE11 does not support passing the array directly to the constructor
-const chartTypeMap = new Map();
-
-chartTypeMap.set('radar', { type: 'line', polar: true });
-chartTypeMap.set('gauge', { type: 'solidgauge' });
-chartTypeMap.set('stackedcolumn', { type: 'column' });
-chartTypeMap.set('stackedbar', { type: 'bar' });
-
-const stackedTypes = ['stackedcolumn', 'stackedbar', 'area'];
+const stackedTypes = ['STACKED_COLUMN', 'STACKEDCOLUMN', 'STACKED_BAR', 'STACKEDBAR',  'AREA'];
 
 export function getIsStacked(type) {
-    return arrayContains(stackedTypes, type.toLowerCase());
+    return arrayContains(stackedTypes, type);
 }
 
-export default function (type) {
-    return chartTypeMap.get(type.toLowerCase()) || {
-        type: type.toLowerCase()
-    };
+export default function(type) {
+    switch (type) {
+        case 'BAR':
+        case 'STACKED_BAR':
+        case 'STACKEDBAR':
+            return { type: 'bar' };
+        case 'LINE':
+            return { type: 'line' };
+        case 'AREA':
+            return { type: 'area' };
+        case 'PIE':
+            return { type: 'pie' };
+        case 'RADAR':
+            return { type: 'line', polar: true };
+        case 'GAUGE':
+            return { type: 'solidgauge' };
+        case 'COLUMN':
+        case 'STACKED_COLUMN':
+        case 'STACKEDCOLUMN':
+        default:
+            return { type: 'column' };
+    }
 }
