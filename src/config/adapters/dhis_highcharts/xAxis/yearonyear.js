057586c0639a10b623ca3dfd94d326a8e68b3463
Edoardo Sabadelli <edoardo@dhis2.org>
2018-10-29 14:34:59 +0100

    Fix X axis labels for year on year charts

--- src/config/adapters/dhis_highcharts/xAxis/yearonyear.js
+++ src/config/adapters/dhis_highcharts/xAxis/yearonyear.js
@@ -1,8 +1,21 @@
 export default function(store, layout) {
-    // use the first response, as the "short name" will be the same in all responses
-    const categories = Object.values(store.data[0].metaData.items).reduce((categories, item) => {
+    let res;
+
+    // look for the response with the longer list of periods.
+    // in some cases (ie. weeks per year) responses might have a different number of periods in metadata
+    store.data.forEach(r => {
+        if (res) {
+            res = r.metaData.dimensions.pe.length > res.metaData.dimensions.pe.length ? r : res;
+        } else {
+            res = r;
+        }
+    });
+
+    const metaData = res.metaData;
+
+    const categories = metaData.dimensions.pe.reduce((categories, periodId) => {
         // TODO use shortName or pass extra option to the request for getting short names in name prop
-        categories.push(item.name);
+        categories.push(metaData.items[periodId].name);
         return categories;
     }, []);
 
