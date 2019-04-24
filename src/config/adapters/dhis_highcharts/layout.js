4868b8460fc22178acf78bd82fc3cab9bc8b87c8
Jan Henrik Overland <jan@dhis2.org>
2019-04-24 11:13:47 +0300

    Add number of items check

--- src/config/adapters/dhis_highcharts/layout.js
+++ src/config/adapters/dhis_highcharts/layout.js
@@ -9,8 +9,16 @@ function isDataSeries(columns) {
         columns[0].dimension === DATA_DIMENSION_ID;
 }
 
+function hasMultipleSeriesItems(columns) {
+    return Array.isArray(columns) &&
+        columns[0] &&
+        Array.isArray(columns[0].items) &&
+        columns[0].items.length > 1;
+}
+
 export function shouldHaveDualAxis(layout) {
     return isDualAxis(layout.type) &&
         isDataSeries(layout.columns) &&
+        hasMultipleSeriesItems(layout.columns) &&
         hasExtraAxisItems(layout.seriesItems, layout.columns);
 }
