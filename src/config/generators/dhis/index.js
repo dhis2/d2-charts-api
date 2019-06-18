46175072f7d05678146c2c44ac23d9a86718f1f2
Edoardo Sabadelli <edoardo@dhis2.org>
2019-06-18 16:16:59 +0200

    feat: add dhis generator for single value

--- src/config/generators/dhis/index.js
+++ src/config/generators/dhis/index.js
@@ -1,12 +1,34 @@
-export default function (chartEl, parentEl) {
-    if (chartEl) {
-        const node = typeof parentEl === 'object' ? parentEl :
-            typeof parentEl === 'string' ? document.querySelector(parentEl) : null;
+import { VISUALIZATION_TYPE_SINGLE_VALUE } from '../../adapters/dhis_dhis/type';
+import getSingleValueGenerator from './singleValue';
+
+export default function(config, parentEl) {
+    if (config) {
+        const node =
+            typeof parentEl === 'object'
+                ? parentEl
+                : typeof parentEl === 'string'
+                ? document.querySelector(parentEl)
+                : null;
 
         if (node) {
-            node.appendChild(chartEl);
-        }
+            if (node.lastChild) {
+                node.removeChild(node.lastChild);
+            }
 
-        return chartEl;
+            // avoid vertical scrollbars
+            node.style.overflow = 'hidden';
+
+            let content;
+
+            switch (config.type) {
+                case VISUALIZATION_TYPE_SINGLE_VALUE:
+                    content = getSingleValueGenerator(config, node);
+                    break;
+                default:
+                    content = getSingleValueGenerator(config, node);
+            }
+
+            node.appendChild(content);
+        }
     }
-};
+}
