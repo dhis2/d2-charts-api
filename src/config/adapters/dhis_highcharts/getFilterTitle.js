82303662d5ce959b5a9597e0c8ecd96b590bf978
Jen Jones Arnesen <jennifer@dhis2.org>
2019-06-24 12:32:55 +0200

    Fix join characters (hyphens, commas)

--- src/config/adapters/dhis_highcharts/getFilterTitle.js
+++ src/config/adapters/dhis_highcharts/getFilterTitle.js
@@ -3,61 +3,44 @@ import { ouIdHelper } from '@dhis2/d2-ui-analytics'
 import i18n from '@dhis2/d2-i18n'
 
 export default function (filters, metaData) {
-    let title;
+    if (isArray(filters)) {
+        const titleParts = [];
 
-    if (isArray(filters) && filters.length) {
-        title = '';
-        
-        let filterItems;
-        let i;
-        let l;
-
-        filters.forEach((filter, index, array) => {
-            const isOuFilterWithLevel = filter.dimension === 'ou' ? filter.items.some(item => ouIdHelper.hasLevelPrefix(item.id)) : false;
+        filters.forEach(filter => {
+            const isOuFilterWithLevels = filter.dimension === 'ou' ? filter.items.some(item => ouIdHelper.hasLevelPrefix(item.id)) : false;
             const isOuFilterWithGroups = filter.dimension === 'ou' ? filter.items.some(item => ouIdHelper.hasGroupPrefix(item.id)) : false;
 
-            if (isOuFilterWithLevel || isOuFilterWithGroups) {
-                const titleParts = [];
-
+            if (isOuFilterWithLevels || isOuFilterWithGroups) {
                 if (isOuFilterWithGroups) {
                     titleParts.push(getOuFilterTitle(filter.items, metaData, false))
                 }
 
-                if (isOuFilterWithLevel) {
+                if (isOuFilterWithLevels) {
                     titleParts.push(getOuFilterTitle(filter.items, metaData, true))
-                }
-
-                title += titleParts.join(' - ')
+                }    
             } else {
-                filterItems = metaData.dimensions[filter.dimension];
+                const filterItems = metaData.dimensions[filter.dimension];
 
                 if (isArray(filterItems)) {
-                    l = filterItems.length;
-                    let id;
+                    const hasMetadata = filterItems.some(id => Boolean(metaData.items[id]));
 
-                    for (i = 0; i < l; i++) {
-                        id = filterItems[i];
-
-                        // if the value is present in items take the name to show from there
-                        if (metaData.items[id]) {
-                            title += metaData.items[id].name + (i < l - 1 ? ', ' : '');
-                        }
-                        // otherwise use the values directly
-                        // this is a temporary fix to avoid app crashing when using filters with data items in EV
-                        else {
-                            title += metaData.items[filter.dimension].name + ': ' + filterItems.join(', ');
-
-                            break;
-                        }
+                    if (hasMetadata) {
+                        const sectionParts = filterItems.map(id => metaData.items[id].name);
+    
+                        titleParts.push(sectionParts.join(', '));
+                    } else {
+                        // use the values directly. This is a temporary fix to prevent
+                        // the EV app from crashing when using filters with data items
+                        titleParts.push(metaData.items[filter.dimension].name + ': ' + filterItems.join(', '));
                     }
-
-                    title += (index < array.length - 1 ? ' - ' : '');
                 }
             }
         });
+
+        return titleParts.join(' - ');
     }
 
-    return title || null;
+    return null;
 }
 
 const getOuFilterTitle = (items, metaData, isLevel) => {
